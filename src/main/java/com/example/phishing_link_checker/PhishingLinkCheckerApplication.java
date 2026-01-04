62920b6f0322f336c3b377c76699c21ea00d3eab
Aleksander <aleksanderp1993@gmail.com>
2026-01-04 19:11:11 +0100

    Init

---
+++ src/main/java/com/example/phishing_link_checker/PhishingLinkCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.phishing_link_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingLinkCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhishingLinkCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phishing_link_checker/PhishingLinkCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phishing_link_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingLinkCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
